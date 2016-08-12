7c43c5557f1769f4f4df1707eab35f33b815caaa
Isaias <impomales@gmail.com>
2016-08-12 20:09:33 +0000

    count lesson done.

---
+++ lessons/count.js
@@ -0,0 +1,18 @@
+var mongo = require('mongodb').MongoClient
+
+mongo.connect('mongodb://localhost:27017/learnyoumongo', function(err, db) {
+    if (err) throw err
+    
+    var collection = db.collection('parrots')
+    
+    collection.count({
+        age: {
+            $gt: parseInt(process.argv[2])
+        }
+    }, function(err, count) {
+        if (err) throw err
+        
+        console.log(count)
+        db.close()
+    })
+})
