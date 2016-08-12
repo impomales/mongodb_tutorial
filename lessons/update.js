dff0a42bed144059181a1900120c3c3b965930bd
Isaias <impomales@gmail.com>
2016-08-12 19:53:25 +0000

    update lesson done.

---
+++ lessons/update.js
@@ -0,0 +1,20 @@
+var mongo = require('mongodb').MongoClient
+
+mongo.connect('mongodb://localhost:27017/' + 
+process.argv[2], function(err, db) {
+    if (err) throw err
+    
+    var collection = db.collection('users')
+    
+    collection.update({
+        username: 'tinatime'
+    }, {
+        $set: {
+            age: 40
+        }
+    }, function(err) {
+        if (err) throw err
+        
+        db.close()
+    })
+})
